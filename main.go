e7cef52dd82bb77a3d5b849bc161bf1ce2e38e00
Xiangyang Li <13622004447@163.com>
2019-06-17 22:48:45 +0800

    init commit

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kangaloo/go-socks-proxy/monitor"
+	"log"
+	"net"
+)
+
+// 加入 prometheus 监控
+// socks 代理
+
+func main() {
+	go monitor.Prometheus()
+
+	server, err := net.Listen("tcp", "[::]:1080")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer server.Close()
+
+	for {
+		conn, err := server.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+
+		fmt.Printf("new connection from %s\n", conn.RemoteAddr())
+		go communicate(conn)
+	}
+}
+
+// 每个communicate协程传入一个channel，用于写入转发的byte数，prom监控读取channel中的数据，实现统计流量
+func communicate(conn net.Conn) {
+	bufSize := 8
+	buf := make([]byte, bufSize)
+
+	defer func() {
+		log.Printf("connection from %s will be closed\n", conn.RemoteAddr())
+		_ = conn.Close()
+	}()
+
+	_, _ = conn.Write([]byte("socks-proxy >> "))
+
+	counter := monitor.NewFlowCounter(conn.RemoteAddr().String())
+	defer counter.Close()
+
+	for {
+		// client 主动关闭时且无数据可读时读到了EOF (TCP收到了断开连接的包)
+		// 当还有没读完的数据，client主动关闭时会怎么样 (猜测会先处理完正常数据包，在处理断开连接的包，因为是流式的)
+		// net包对底层做了封装，在TCP连接处于某种状态时向上层返回EOF，比如TCP连接收到断开指令时
+		n, err := conn.Read(buf)
+		if err != nil {
+			log.Printf("%#v", err)
+			log.Println(err)
+			return
+		}
+
+		//monitor.FlowCounter.Write(n)
+		counter.Write(n)
+
+		fmt.Printf("%s, %d\n", buf[:n], n)
+		if n < bufSize {
+			_, _ = conn.Write([]byte("socks-proxy >> "))
+			//break
+		}
+	}
+}
---
+++ monitor/g.go
@@ -0,0 +1,145 @@
+package monitor
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
+	"net/http"
+	"sync"
+)
+
+func Prometheus() {
+	/*
+		counter := &Counter{
+			Zone: "zone",
+			Flow: prometheus.NewDesc(
+				"socks_proxy",
+				"GO-SOCKS-PROXY TOTAL FLOW",
+				[]string{"proxy"},
+				map[string]string{"zone": "zone"},
+			),
+		}
+
+		err := prometheus.Register(counter)
+		if err != nil {
+			log.Println(err)
+		}
+
+	*/
+
+	http.HandleFunc("/metrics", promhttp.Handler().ServeHTTP)
+	err := http.ListenAndServe("[::]:8080", http.DefaultServeMux)
+	log.Panic(err)
+}
+
+type Counter struct {
+	sync.Mutex
+	Zone       string
+	Address    string
+	TotalBytes int
+	Ch         chan int
+	Flow       *prometheus.Desc
+	finished   bool
+}
+
+func NewFlowCounter(addr string) *Counter {
+	counter := &Counter{
+		Zone:       "zone",
+		Address:    addr,
+		TotalBytes: 0,
+		Ch:         make(chan int, 1024),
+		Flow: prometheus.NewDesc(
+			"SOCKS_PROXY",
+			"GO-SOCKS-PROXY TOTAL FLOW",
+			[]string{"flow", "proxy"},
+			map[string]string{"ADDR": addr},
+		),
+	}
+	err := prometheus.Register(counter)
+	if err != nil {
+		log.Println(err)
+	}
+
+	go counter.Count()
+	return counter
+}
+
+func (c *Counter) Count() {
+	for {
+		n, ok := <-c.Ch
+		if !ok {
+			break
+		}
+
+		log.Printf("get %d from channel\n", n)
+		c.Lock()
+		c.TotalBytes += n
+		c.Unlock()
+	}
+	c.finished = true
+
+	log.Printf("counter for %s exited", c.Address)
+}
+
+func (c *Counter) UnRegister() {
+	log.Printf("UnRegister metric successfully: %v", prometheus.Unregister(c))
+}
+
+func (c *Counter) Write(n int) {
+	c.Ch <- n
+	log.Printf("write %d bytes, the total is %d bytes\n", n, c.TotalBytes)
+}
+
+func (c *Counter) Read() int {
+	c.Lock()
+	count := c.TotalBytes
+	//c.TotalBytes = 0
+	c.Unlock()
+	log.Printf("read %d bytes", count)
+	if c.finished {
+		c.UnRegister()
+	}
+	return count
+}
+
+func (c *Counter) Close() {
+	close(c.Ch)
+}
+
+func (c *Counter) Describe(ch chan<- *prometheus.Desc) {
+	ch <- c.Flow
+}
+
+func (c *Counter) Collect(ch chan<- prometheus.Metric) {
+	/*
+		count := FlowCounter.Read()
+		counter, err := prometheus.NewConstMetric(
+			c.Flow,
+			prometheus.CounterValue,
+			float64(count),
+			"flow",
+		)
+
+		if err != nil {
+			log.Println(err)
+		}
+
+		ch <- counter
+
+	*/
+
+	count := c.Read()
+	counter, err := prometheus.NewConstMetric(
+		c.Flow,
+		prometheus.CounterValue,
+		float64(count),
+		"",
+		"",
+	)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	ch <- counter
+}
