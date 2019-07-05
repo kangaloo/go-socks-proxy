8be4b11d0708ad647ec643c247f57849fc30c29c
Xiangyang Li <13622004447@163.com>
2019-07-05 16:12:48 +0800

    重构Socks函数

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/kangaloo/go-socks-proxy
 go 1.12
 
 require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/prometheus/client_golang v0.9.4
 	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/viper v1.3.2
--- protocol/util.go
+++ protocol/util.go
@@ -3,33 +3,10 @@ package protocol
 import (
 	"bytes"
 	"encoding/binary"
-	"github.com/pkg/errors"
+	"errors"
 	"net"
 	"strconv"
 )
-
-func sliceEqual(a, b []byte) bool {
-	if len(a) != len(b) {
-		return false
-	}
-	if (a == nil) != (b == nil) {
-		return false
-	}
-	for i, v := range a {
-		if v != b[i] {
-			return false
-		}
-	}
-	return true
-}
-
-func parseDomain() {
-
-}
-
-func parseIP() {
-
-}
 
 // 生成返回给客户端的包
 func genPacket(method byte) []byte {
